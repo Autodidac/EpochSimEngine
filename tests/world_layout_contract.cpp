d00068283e89b4ad1dcc1b9752d88980f93c9c35
Adam Rushford <177971875+Autodidac@users.noreply.github.com>
2026-08-02 05:32:50 -0400

    Correct exact geology boundary contract samples

--- tests/world_layout_contract.cpp
+++ tests/world_layout_contract.cpp
@@ -8,13 +8,13 @@ static_assert(subterranean_zone_count == 3u);
 static_assert(authored_scene_foundation_cells == 8u);
 static_assert(resident_world_lava_cells == 16u);
 
-static_assert(resident_substrate_material(resident_world_width, resident_world_height, 1280u, 351u) == Material::stone);
+static_assert(resident_substrate_material(resident_world_width, resident_world_height, 1280u, 352u) == Material::stone);
 static_assert(resident_substrate_material(resident_world_width, resident_world_height, 1280u, 360u) == Material::sand);
 static_assert(resident_substrate_material(resident_world_width, resident_world_height, 1280u, 480u) == Material::dirt);
 static_assert(resident_substrate_material(resident_world_width, resident_world_height, 1280u, 610u) == Material::silt);
 static_assert(resident_substrate_material(resident_world_width, resident_world_height, 1280u, 720u) == Material::dirt);
 static_assert(resident_substrate_material(resident_world_width, resident_world_height, 1280u, 870u) == Material::silt);
-static_assert(resident_substrate_material(resident_world_width, resident_world_height, 1280u, 1020u) == Material::sand);
+static_assert(resident_substrate_material(resident_world_width, resident_world_height, 1344u, 1020u) == Material::sand);
 static_assert(resident_substrate_material(resident_world_width, resident_world_height, 1280u, 1080u) == Material::silt);
 static_assert(resident_substrate_material(resident_world_width, resident_world_height, 1280u, 1180u) == Material::dirt);
 static_assert(resident_substrate_material(resident_world_width, resident_world_height, 1280u, 1330u) == Material::stone);
