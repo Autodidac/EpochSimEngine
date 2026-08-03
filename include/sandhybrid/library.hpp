7e21fdde14e6ec8b4b40d38f9f24ae1e0782f4d0
Adam Rushford <177971875+Autodidac@users.noreply.github.com>
2026-08-02 20:36:02 -0400

    Advance reusable core API to v3

--- include/sandhybrid/library.hpp
+++ include/sandhybrid/library.hpp
@@ -1,5 +1,10 @@
 #pragma once
 
+#include "sandhybrid/actor_medium.hpp"
+#include "sandhybrid/atmosphere.hpp"
+#include "sandhybrid/inventory.hpp"
+#include "sandhybrid/machinery.hpp"
+#include "sandhybrid/packet_transaction.hpp"
 #include "sandhybrid/scene_image.hpp"
 #include "sandhybrid/section_grid.hpp"
 #include "sandhybrid/section_scheduler.hpp"
@@ -10,13 +15,17 @@
 
 namespace sandhybrid {
 
-inline constexpr std::uint32_t library_api_version = 2u;
+inline constexpr std::uint32_t library_api_version = 3u;
 inline constexpr std::string_view library_name = "SandHybrid";
 
 struct LibraryCapabilities final {
     bool native_startup_owned_by_consumer{true};
     bool windowing_required{false};
     bool vulkan_required{false};
+    bool packed_atmosphere_available{true};
+    bool transactional_packets_available{true};
+    bool actor_medium_contracts_available{true};
+    bool machinery_transactions_available{true};
 };
 
 inline constexpr LibraryCapabilities core_library_capabilities{};
