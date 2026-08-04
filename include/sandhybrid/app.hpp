eb6e31e8ec8f6130d98cdd45b564f6afa97c3117
Adam Rushford <177971875+Autodidac@users.noreply.github.com>
2026-08-04 13:21:58 -0400

    Add startup world-size and save-slot options

--- include/sandhybrid/app.hpp
+++ include/sandhybrid/app.hpp
@@ -1,7 +1,16 @@
 #pragma once
 
+#include "sandhybrid/camera_policy.hpp"
+
+#include <string>
+
 namespace sandhybrid {
 
-int run_application();
+struct ApplicationOptions final {
+    WorldSizePreset world_size{WorldSizePreset::large};
+    std::string save_slot{"quick"};
+};
+
+int run_application(const ApplicationOptions& options);
 
 } // namespace sandhybrid
